812174249d39f4a74a256339bd7e9057f3df9c43
kanashiro <willianatsuki@gmail.com>
2014-05-31 16:00:37 -0300

    Primeiro Commit para teste

--- TrabalhoProlog/src/trabalhoprolog/TrabalhoProlog.java
+++ TrabalhoProlog/src/trabalhoprolog/TrabalhoProlog.java
@@ -4,6 +4,10 @@
  */
 package trabalhoprolog;
 
+import mProlog.PrologEngine;
+import mProlog.PrologQuery;
+import mProlog.PrologTerm;
+
 /**
  *
  * @author WillianKanashiro
@@ -14,6 +18,32 @@ public class TrabalhoProlog {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // escrever o arquivo
+		 PrologEngine baseProlog = new PrologEngine(); 
+		 baseProlog.add("estuda(rafael,turma2015)");
+		 baseProlog.add("estuda(kanashiro,turma2015)");
+		 baseProlog.add("estuda(marcella,turma2015)");
+		 baseProlog.add("estuda(leonardo,turma2008)");
+		 baseProlog.add("estuda(harryson,turma2009)");
+
+		 baseProlog.add("colegas(A,B):-estuda(A,C),estuda(B,C)"); 
+
+		 // escrever as minhas perguntas
+		 PrologTerm pergunta1 = PrologTerm.create("colegas(A,B)"); 
+		 PrologTerm pergunta2 = PrologTerm.create("estuda(X,turma2015)"); 
+		 
+		 // executar as queries
+		 PrologQuery query = new PrologQuery(baseProlog, pergunta2);
+		 PrologTerm[] solution = query.solution(); 
+		 
+                 
+                
+                 while(solution != null) 
+		 {
+			 System.out.println(solution[0].getVariableValue()); 
+                         solution = query.solution();
+
+		 }
+                 
     }
 }
