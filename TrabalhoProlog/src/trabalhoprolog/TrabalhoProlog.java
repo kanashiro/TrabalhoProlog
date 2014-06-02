b33075582eccb0c50e177ab7e01ca667c69704c3
kanashiro <willianatsuki@gmail.com>
2014-06-02 02:59:42 -0300

    Adicionado parte gráfica

--- TrabalhoProlog/src/trabalhoprolog/TrabalhoProlog.java
+++ TrabalhoProlog/src/trabalhoprolog/TrabalhoProlog.java
@@ -18,31 +18,7 @@ public class TrabalhoProlog {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // escrever o arquivo
-		 PrologEngine baseProlog = new PrologEngine(); 
-		 baseProlog.add("estuda(rafael,turma2015)");
-		 baseProlog.add("estuda(kanashiro,turma2015)");
-		 baseProlog.add("estuda(marcella,turma2015)");
-		 baseProlog.add("estuda(leonardo,turma2008)");
-		 baseProlog.add("estuda(harryson,turma2009)");
-
-		 baseProlog.add("colegas(A,B):-estuda(A,C),estuda(B,C)"); 
-
-		 // escrever as minhas perguntas
-		 PrologTerm pergunta1 = PrologTerm.create("colegas(A,B)"); 
-		 PrologTerm pergunta2 = PrologTerm.create("estuda(X,turma2015)"); 
-		 
-		 // executar as queries
-		 PrologQuery query = new PrologQuery(baseProlog, pergunta2);
-		 PrologTerm[] solution = query.solution(); 
-		 
-                 System.out.println("o");
-                 while(solution != null) 
-		 {
-			 System.out.println(solution[0].getVariableValue()); 
-                         solution = query.solution();
-
-		 }
-                 
+     
+          TrabalhoDamas_Tabuleiro.main(args);       
     }
 }
